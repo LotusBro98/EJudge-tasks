0c3ffe692312c1279f70fc8f39539e2ac62b40bc
LotusBro98 <LotusBro98@yandex.ru>
2017-01-13 14:59:39 +0300

    solving task "complex"

--- C++/Semestr++/complex.cpp
+++ C++/Semestr++/complex.cpp
@@ -2,3 +2,15 @@
 // Created by Alexpan98 on 13.01.2017.
 //
 
+class C
+{
+    public:
+        double re;
+        double im;
+
+    C(double re, double im)
+    {
+        this->re = re;
+        this->im = im;
+    }
+};
